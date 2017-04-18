1cfa469296b93a480e2264d383f17dbc79a7f74f
Emmali Hanson <emmali.hanson@mu.edu>
2017-04-18 15:15:30 -0500

    fixed some formatting

--- main.cpp
+++ main.cpp
@@ -11,42 +11,51 @@ void printM(int ***data, int h, int w, int r);
 void initialize(int ***array, int height, int width, int range);
 
 
-int main(int argc, char** argv) {
-	
-	int HEIGHT = 2;
-	int WIDTH = 2;
+int main(int argc, char** argv) 
+{
+	//error checking command line args
+	if (argc != 4)
+	{
+		printf("Error: please enter 3 command line arguments %i\n", argc);
+	}
+	int HEIGHT = atoi(argv[2]);
+	int WIDTH = atoi(argv[3]);
 	int RANGE = 3;
-	
+
 	printf("Hello world\n");
-	
+
 	int ***data;
 
-  	// Allocate memory
-  	data = new int**[HEIGHT];  // layer 1
-  	for (int i = 0; i < HEIGHT; ++i) {  // layer 2
-    	data[i] = new int*[WIDTH];
-    	for (int j = 0; j < WIDTH; ++j)  // layer 3
-    		data[i][j] = new int[RANGE];
-  	}	
+	// Allocate memory
+	data = new int**[HEIGHT];  // layer 1
+	for (int i = 0; i < HEIGHT; ++i) 
+	{  // layer 2
+		data[i] = new int*[WIDTH];
+		for (int j = 0; j < WIDTH; ++j)  // layer 3
+		{
+			data[i][j] = new int[RANGE];
+		}
+	}	
 	initialize(data, HEIGHT, WIDTH, RANGE);
-	
+
 	printM(data, HEIGHT, WIDTH, RANGE);
 	printf("DONE PRINTING\n");
-	
+
 	printM(data, HEIGHT, WIDTH, RANGE);
-	
+
 	// De-Allocate memory to prevent memory leak
-  	for (int i = 0; i < HEIGHT; ++i) {
-    	for (int j = 0; j < WIDTH; ++j)
-      		delete [] data[i][j];
+	for (int i = 0; i < HEIGHT; ++i) 
+	{
+		for (int j = 0; j < WIDTH; ++j)
+		{
+			delete [] data[i][j];
+		}
+		delete [] data[i];
+	}
+	delete [] data;
+	printf("DEALLOCATED\n");
 
-    	delete [] data[i];
-  	}
-  	delete [] data;
-  	printf("DEALLOCATED\n");
-	
 	return 0;
-
 }
 
 
@@ -64,14 +73,18 @@ void initialize(int ***array, int height, int width, int range)
 			}
 		}
 	}
-	
+
 	cout << "end of init" << endl;
 }
 
-void printM(int ***data, int h, int w, int r) {
-	for(int i = 0; i < h; i++) {
-		for(int j = 0; j < w; j++) {
-			for(int k = 0; k < r; k++) {
+void printM(int ***data, int h, int w, int r) 
+{
+	for(int i = 0; i < h; i++) 
+	{
+		for(int j = 0; j < w; j++) 
+		{
+			for(int k = 0; k < r; k++) 
+			{
 				printf("[%d][%d][%d] = %d | ", i, j, k, data[i][j][k]);
 			}
 			printf("\n");
